3062df979f4aa3373432d03e94ccc79138619d43
JuanPetrik <juanpablopetrik@gmail.com>
2016-11-10 23:39:53 -0200

    Parte 07 - Realizando cadastro através de importação de XML.
    Foi adicionado um método para realizar a importação do cadastro de
    pessoas através de um xml.

--- AppJsf/src/main/java/br/com/petrik/pessoa/controller/CadastrarPessoaController.java
+++ AppJsf/src/main/java/br/com/petrik/pessoa/controller/CadastrarPessoaController.java
@@ -1,8 +1,20 @@
 package br.com.petrik.pessoa.controller;
 
+import java.io.IOException;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.primefaces.model.UploadedFile;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import br.com.petrik.model.PessoaModel;
 import br.com.petrik.repository.PessoaRepository;
@@ -29,6 +41,16 @@ public class CadastrarPessoaController {
 	@Inject
 	PessoaRepository pessoaRepository;
 
+	private UploadedFile file;
+
+	public UploadedFile getFile() {
+		return file;
+	}
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+
 	public PessoaModel getPessoaModel() {
 		return pessoaModel;
 	}
@@ -38,7 +60,7 @@ public class CadastrarPessoaController {
 	}
 
 	/**
-	 * Salvando um novo registro por input
+	 *  Método responsável por salvar um novo registro
 	 */
 	public void SalvarNovaPessoa() {
 
@@ -54,4 +76,68 @@ public class CadastrarPessoaController {
 
 	}
 
+	/**
+	 * Aqui que vamos fazer o upload do arquivo xml
+	 */
+	public void UploadRegistros() {
+
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+		try {
+
+			if (this.file.getFileName().equals("")) {
+				Uteis.MensagemAtencao("Nenhum arquivo selecionado!");
+				return;
+			}
+
+			DocumentBuilder builder = factory.newDocumentBuilder();
+
+			Document doc = builder.parse(this.file.getInputstream());
+
+			Element element = doc.getDocumentElement();
+
+			NodeList nodes = element.getChildNodes();
+
+			for (int i = 0; i < nodes.getLength(); i++) {
+
+				Node node = nodes.item(i);
+
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element elementPessoa = (Element) node;
+
+					// PEGANDO OS VALORES DO ARQUIVO XML
+					String nome = elementPessoa.getElementsByTagName("nome").item(0).getChildNodes().item(0)
+							.getNodeValue();
+					String sexo = elementPessoa.getElementsByTagName("sexo").item(0).getChildNodes().item(0)
+							.getNodeValue();
+					String email = elementPessoa.getElementsByTagName("email").item(0).getChildNodes().item(0)
+							.getNodeValue();
+					String endereco = elementPessoa.getElementsByTagName("endereco").item(0).getChildNodes().item(0)
+							.getNodeValue();
+
+					PessoaModel newPessoaModel = new PessoaModel();
+
+					newPessoaModel.setUsuarioModel(this.usuarioController.GetUsuarioSession());
+					newPessoaModel.setEmail(email);
+					newPessoaModel.setEndereco(endereco);
+					newPessoaModel.setNome(nome);
+					newPessoaModel.setOrigemCadastro("X");
+					newPessoaModel.setSexo(sexo);
+
+					pessoaRepository.SalvarNovoRegistro(newPessoaModel);
+				}
+			}
+
+			Uteis.MensagemInfo("Registros cadastrados com sucesso!");
+
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
